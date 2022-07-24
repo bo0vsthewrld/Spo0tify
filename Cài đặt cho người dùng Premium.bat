482d563de3c8dce472f12f866596c64cb7ea3667
bo0vsthewrld <52617769+bo0vsthewrld@users.noreply.github.com>
2022-07-25 05:06:53 +0700

    Add files via upload

---
+++ Cài đặt cho người dùng Premium.bat
@@ -0,0 +1,6 @@
+@echo off
+
+powershell -Command "&{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12}; """"& { $((Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/').Content)} -premium -start_spoti """" | Invoke-Expression"
+
+pause
+exit /b
